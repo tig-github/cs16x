3043a5306927e3e6b0b94ac7a43601a71031a1b3
Rayan Tighiouart <t.alex.2002@gmail.com>
2024-02-12 16:07:14 -0800

    CS 163 astar tests

--- CS 163/tests/shortestpath_tests.py
+++ CS 163/tests/shortestpath_tests.py
@@ -30,5 +30,18 @@ class TestMinSpanTrees(unittest.TestCase):
         self.assertEqual([1, 0, 1, 2, 3, 4, 5, 6], distances)
         self.assertEqual([1, None, 1, 2, 3, 4, 5, 6], predecessors)
         
+    # A* Algorithm
+    def test_astar_null(self):
+        graph = {}
+        distances,predecessors = a_star('', graph, lambda x,y:1)
+        self.assertEqual([], distances)
+        self.assertEqual([], predecessors)
+        
+    def test_astar_straightgraph_empty_heuristic(self):
+        graph = {1: [(2, 1), (0, 1)], 2: [(1, 1), (3, 1)], 3: [(2, 1), (4, 1)], 4: [(3, 1), (5, 1)], 5: [(4, 1), (6, 1)], 6: [(5, 1), (7, 1)], 7: [(6, 1)], 0: [(1, 1)]}
+        distances,predecessors = a_star(1, graph, lambda x,y:1)
+        self.assertEqual([1, 0, 1, 2, 3, 4, 5, 6], distances)
+        self.assertEqual([1, None, 1, 2, 3, 4, 5, 6], predecessors)
+        
 if __name__ == '__main__':
     unittest.main()
